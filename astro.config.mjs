836a53a833c7f79e4c32aa0812120016a15e57bb
John Fay <jfay@miletwo.us>
2024-08-02 23:33:38 -0400

    initial astro sphere data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 import { defineConfig } from "astro/config";
-
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://keonik.github.io/",
-  base: "astro-demo",
-  integrations: [tailwind()],
+  site: "https://keonik.github.io/astro-demo",
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [[remarkToc, { heading: "toc" }]],
+    rehypePlugins: [
+      rehypeAccessibleEmojis,
+      [rehypeHeadingIds, { behavior: "append" }],
+    ],
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    solidJs(),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
